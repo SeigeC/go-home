579577beb699e45529fa4fecd14ef10b8a6695a9
过客龙门 <486315944@qq.com>
2024-08-30 11:30:18 +0800

    支持多系统 (#1)
    * 支持多系统
    
    * 调整写法

--- main.go
+++ main.go
@@ -1,49 +1,41 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
-	"os/exec"
-	"strings"
+	"log/slog"
 	"time"
+
+	"go-home/os"
 )
 
+type OS interface {
+	GetUnlockTime() (time.Time, error)
+	Notify(unlockTime, reminderTime time.Time) error
+}
+
+type Runner struct {
+	OS OS
+}
+
 func main() {
-	// 获取今天的解锁时间，不包括毫秒和时区偏移量
-	dateCmd := `log show --predicate='(process == "loginwindow") AND (composedMessage endswith "unlock success")' --info --style syslog --start "$(date '+%Y-%m-%d') 00:00:00" --end "$(date '+%Y-%m-%d') 23:59:59" | grep 'unlock success' | head -n 1 | awk '{print $1, substr($2,1,8)}'`
-
-	cmd := exec.Command("sh", "-c", dateCmd)
-	var out bytes.Buffer
-	cmd.Stdout = &out
-	err := cmd.Run()
+	runner := Runner{OS: os.RunnerOS}
+	unlockTime, err := runner.OS.GetUnlockTime()
 	if err != nil {
-		fmt.Println("Error executing command:", err)
+		slog.Error("获取解锁时间失败：", slog.Any("err", err))
 		return
 	}
 
-	// 提取解锁时间
-	unlockTimeStr := strings.TrimSpace(out.String())
-	fmt.Println("Unlock time:", unlockTimeStr)
-
-	// 解析解锁时间
-	unlockTime, err := time.Parse(time.DateTime, unlockTimeStr)
-	if err != nil {
-		fmt.Println("Error parsing unlock time:", err)
-		return
-	}
+	slog.Info("解锁时间", slog.String("time", unlockTime.Format(time.DateTime)))
 
 	// 计算 9 小时后的时间
 	reminderTime := unlockTime.Add(9 * time.Hour)
-	fmt.Println("提醒时间：", reminderTime.Format(time.DateTime))
+	slog.Info("提醒时间", slog.String("time", reminderTime.Format(time.DateTime)))
 
 	<-time.After(time.Until(reminderTime))
 
-	// 设置提醒
-	osascriptCmd := fmt.Sprintf(`osascript -e 'display notification "%s %s - %s 已工作满 8 小时" with title "准备下班"'`, unlockTime.Format(time.DateOnly), unlockTime.Format(time.Kitchen), reminderTime.Format(time.Kitchen))
-	cmd = exec.Command("sh", "-c", osascriptCmd)
-	err = cmd.Run()
+	// 调用系统通知
+	err = runner.OS.Notify(unlockTime, reminderTime)
 	if err != nil {
-		fmt.Println("Error setting reminder:", err)
+		slog.Error("Error setting reminder:", slog.Any("err", err))
 		return
 	}
 }
