c7d497a5e2aea2ffccc0c667c45082794235ecd5
chengyuhan <486315944@qq.com>
2024-08-30 11:35:12 +0800

    fix

--- os/os.go
+++ os/os.go
@@ -1,14 +1,8 @@
 package os
 
 import (
-	"os"
 	"time"
 )
-
-func init() {
-	// 设置环境变量 TZ 为东八区（Asia/Shanghai）
-	os.Setenv("TZ", "Asia/Shanghai")
-}
 
 type OS interface {
 	GetUnlockTime() (time.Time, error)
